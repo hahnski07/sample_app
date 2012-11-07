9a7590d1a1791836c71c5911eb20e05353eeed52
Micah Hahn <mhahn@cse.taylor.edu>
2012-11-06 23:09:12 -0500

    10.3.3 - A proto-feed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -147,5 +147,24 @@ describe User do
 	it "should have the right microposts in the right order" do
 		@user.microposts.should == [newer_micropost, older_micropost]
 	end
+	
+	it "should destroy associated microposts" do
+		microposts = @user.microposts.dup
+		@user.destroy
+		microposts.should_not be_empty
+		microposts.each do |micropost|
+			Micropost.find_by_id(micropost.id).should be_nil
+		end
+	end
+	
+	describe "status" do
+		let(:unfollowed_post) do
+			FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+		end
+		
+		its(:feed) { should include(newer_micropost) }
+		its(:feed) { should include(older_micropost) }
+		its(:feed) { should_not include(unfollowed_post) }
+	end
   end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,7 @@ require 'spec_helper'
 
 describe 'StaticPages' do
 
-	let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
-
 	subject { page }
-	
-	shared_examples_for 'all static pages' do
-		it { should have_selector('h1', text: heading) }
-		it { should have_selector('title', text: full_title(page_title)) }
-	end
 	
 	describe 'Home page' do
 		before { visit root_path }
@@ -18,6 +11,22 @@ describe 'StaticPages' do
 	
 		it_should_behave_like 'all static pages'
 		it { should_not have_selector 'title', text: '| Home' }
+		
+		describe "for signed-in users" do
+			let(:user) { FactoryGirl.create(:user) }
+			before do
+				FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+				FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+				sign_in user
+				visit root_path
+			end
+			
+			it "should render the user's feed" do
+				user.feed.each do |item|
+					page.should have_selector("li##{item.id}", text: item.content)
+				end
+			end
+		end
 	end
   
 	describe 'Help page' do
