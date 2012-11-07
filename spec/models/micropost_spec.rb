df03e8bacb64882ee6a20ceab338d8101ada8806
Micah Hahn <mhahn@cse.taylor.edu>
2012-11-06 22:56:20 -0500

    10.3.2 - Creating microposts

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -23,16 +23,8 @@ describe Micropost do
 		it { should_not be_valid }
 	end
 	
-	describe "with content that is too long"
+	describe "with content that is too long" do
 		before { @micropost.content = "a" * 141 }
 		it { should_not be_valid }
-	end
-	
-	describe "accessible attributes" do
-		it "should not allow access to user_id" do
-			expect do
-				Micropost.new(user_id: user.id)
-			end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
-		end
 	end
 end
